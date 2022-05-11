c964d5574c36edcb7d986c2f184dffd2bba98c3f
a-irizi <irizi.anass.00@gmail.com>
2022-05-11 20:09:17 +0100

    Create model Universite, and register in admin panel

--- gestion_documents_scientifiques/etablissements/models.py
+++ gestion_documents_scientifiques/etablissements/models.py
@@ -1,3 +1,10 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+class Universite(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+    nom = models.CharField("Nom d'Université", max_length=200, null=False, blank=False)
+
+    def __str__(self) -> str:
+        return self.nom
