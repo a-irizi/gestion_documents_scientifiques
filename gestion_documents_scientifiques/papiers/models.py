ade72fbf103c843504cc5865d5db10925743868f
a-irizi <irizi.anass.00@gmail.com>
2022-05-11 19:19:34 +0100

    Create model PublicationRevueInternational, and register in admin panel

--- gestion_documents_scientifiques/papiers/models.py
+++ gestion_documents_scientifiques/papiers/models.py
@@ -9,6 +9,14 @@ class Papier(models.Model):
         AUTRE = "AUTRE", "Autre"
         AUCUNE = "AUCUNE", "Aucune"
     id = models.UUIDField(uuid.uuid4, editable=False, primary_key=True)
-    titre = models.CharField(max_length=200, null=False, blank=False)
-    indexation = models.CharField(max_length=50, choices=Index.choices, null=False, blank=False)
+    titre = models.CharField("Titre", max_length=200, null=False, blank=False)
+    indexation = models.CharField("Indexation", max_length=50, choices=Index.choices, null=False, blank=False)
     est_valide = models.BooleanField(default=False, editable=False)
+
+class PublicationRevueInternational(Papier):
+    nomJournal = models.CharField("Nom Du Journal", max_length=200, null=False, blank=False)
+    numeroJournal = models.PositiveIntegerField("Numéro Du Journal", null=False, blank=False)
+    volumeJournal = models.PositiveIntegerField("Volume", null=False, blank=False)
+    anneePublication = models.PositiveSmallIntegerField("Année de Publication", null=False, blank=False)
+    pageDébut = models.PositiveIntegerField("Page de Début", null=False, blank=False)
+    pageFin = models.PositiveIntegerField("Page de Fin", null=False, blank=False)
