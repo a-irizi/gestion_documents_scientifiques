e9471b3388738b53ad006cdeabe89a2218c11b52
a-irizi <irizi.anass.00@gmail.com>
2022-05-11 19:13:16 +0100

    Create model Papier

--- gestion_documents_scientifiques/papiers/models.py
+++ gestion_documents_scientifiques/papiers/models.py
@@ -1,3 +1,14 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+class Papier(models.Model):
+    class Index(models.TextChoices):
+        SCOPUS = "SCOPUS", "Scopus"
+        WEB_OF_SCIENCE = "WEB_OF_SCIENCE", "Web Of Science"
+        AUTRE = "AUTRE", "Autre"
+        AUCUNE = "AUCUNE", "Aucune"
+    id = models.UUIDField(uuid.uuid4, editable=False, primary_key=True)
+    titre = models.CharField(max_length=200, null=False, blank=False)
+    indexation = models.CharField(max_length=50, choices=Index.choices, null=False, blank=False)
+    est_valide = models.BooleanField(default=False, editable=False)
