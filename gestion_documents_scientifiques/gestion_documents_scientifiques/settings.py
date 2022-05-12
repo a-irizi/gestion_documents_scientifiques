4a16e44e10e1b7724c07452964e117d0e7482aea
a-irizi <irizi.anass.00@gmail.com>
2022-05-12 17:29:18 +0100

    Added email configuration

--- gestion_documents_scientifiques/gestion_documents_scientifiques/settings.py
+++ gestion_documents_scientifiques/gestion_documents_scientifiques/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -115,6 +116,15 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+# Email specific config
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get("GESTION_PAPIER_SCIENTIFIQUE_EMAIL")
+EMAIL_HOST_PASSWORD = os.environ.get("GESTION_PAPIER_SCIENTIFIQUE_PASSWORD")
+
 
 
 # Static files (CSS, JavaScript, Images)
