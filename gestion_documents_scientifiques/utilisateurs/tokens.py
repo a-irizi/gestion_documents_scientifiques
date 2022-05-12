650566c990fda52444c7d09e6abd7f7124ed46b9
a-irizi <irizi.anass.00@gmail.com>
2022-05-12 17:53:48 +0100

    Added token generation class

---
+++ gestion_documents_scientifiques/utilisateurs/tokens.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+
+
+class TokenGenerator(PasswordResetTokenGenerator):
+    def _make_hash_value(self, user, timestamp):
+        return f"{user.pk}{timestamp}{user.emailValide}{user.utilisateur.email}"
+
+
+account_activation_token = TokenGenerator()
