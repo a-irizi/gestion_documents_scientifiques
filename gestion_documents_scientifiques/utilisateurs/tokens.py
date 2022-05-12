ae65eb2f30ed50d5fb9628f2d08e1f2ee5ed73d9
a-irizi <irizi.anass.00@gmail.com>
2022-05-13 00:18:00 +0100

    Change _make_hash_value to use the utilisateur model directly
    instead of passing the chercheur model, pass the utilisateur and use
    reverse queries to access the child models

--- gestion_documents_scientifiques/utilisateurs/tokens.py
+++ gestion_documents_scientifiques/utilisateurs/tokens.py
@@ -3,7 +3,7 @@ from django.contrib.auth.tokens import PasswordResetTokenGenerator
 
 class TokenGenerator(PasswordResetTokenGenerator):
     def _make_hash_value(self, user, timestamp):
-        return f"{user.pk}{timestamp}{user.emailValide}{user.utilisateur.email}"
+        return f"{user.pk}{timestamp}{user.chercheur.emailValide}{user.email}"
 
 
 emailActivationToken = TokenGenerator()
