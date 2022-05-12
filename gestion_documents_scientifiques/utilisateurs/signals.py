876f4c16d9de70a3254193a61341fbb2e0433762
a-irizi <irizi.anass.00@gmail.com>
2022-05-12 22:04:04 +0100

    Fix Signals not working
    instead of removing a chercheur which will trigger the chercheurDeleted
    function. delete the utilisateur directly

--- gestion_documents_scientifiques/utilisateurs/signals.py
+++ gestion_documents_scientifiques/utilisateurs/signals.py
@@ -1,16 +1,20 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_delete
 
-from .models import Thesard, Professeur, DirecteurLabo
+from .models import Chercheur, Thesard, Professeur, DirecteurLabo
+
+@receiver(post_delete, sender=Chercheur)
+def chercheurDeleted(sender, instance, **kwargs):
+    instance.utilisateur.delete()
 
 @receiver(post_delete, sender=Thesard)
 def thesardDeleted(sender, instance, **kwargs):
-    instance.utilisateur.delete()
+    instance.chercheur.utilisateur.delete()
 
 @receiver(post_delete, sender=Professeur)
-def thesardDeleted(sender, instance, **kwargs):
-    instance.utilisateur.delete()
+def professeurDeleted(sender, instance, **kwargs):
+    instance.chercheur.utilisateur.delete()
 
 @receiver(post_delete, sender=DirecteurLabo)
-def thesardDeleted(sender, instance, **kwargs):
-    instance.utilisateur.delete()
+def directeurLaboDeleted(sender, instance, **kwargs):
+    instance.chercheur.utilisateur.delete()
