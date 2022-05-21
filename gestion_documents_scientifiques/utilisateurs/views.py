980bbcbb1970a5ccaa216569d0704eba1a924ab6
a-irizi <irizi.anass.00@gmail.com>
2022-05-21 15:11:21 +0100

    Add views for Thesard account validation

--- gestion_documents_scientifiques/utilisateurs/views.py
+++ gestion_documents_scientifiques/utilisateurs/views.py
@@ -6,10 +6,10 @@ from django.template.loader import render_to_string
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.core.mail import EmailMessage
 
-from utilisateurs.models import Chercheur
+from .models import Chercheur, Professeur, Thesard
 
 from .forms import ChercheurForm, ProfesseurForm, ThesardForm
-from .tokens import emailActivationToken
+from .tokens import tokenGenerator
 
 # Create your views here.
 def register(request):
@@ -21,14 +21,29 @@ def sendEmailConfirmationEmail(user, toEmail):
         'user': user,
         'domain': '127.0.0.1:8000',
         'uid': urlsafe_base64_encode(force_bytes(user.chercheur.utilisateur.pk)),
-        'token': emailActivationToken.make_token(user),
+        'token': tokenGenerator.make_token(user),
     })
-    print(message)
     email = EmailMessage(
                 mail_subject, message, to=[toEmail]
     )
     email.content_subtype = "html"
     email.send()
+
+def sendThesardAccountConfirmationEmail(user, toEmail):
+    mail_subject = 'Valider Un compte thesard'
+    message = render_to_string('utilisateurs/thesard-account-confirmation-email.html', {
+        'user': user,
+        'domain': '127.0.0.1:8000',
+        'uid1': urlsafe_base64_encode(force_bytes(user.chercheur.thesard.directeurThese.chercheur.utilisateur.pk)),
+        'uid2': urlsafe_base64_encode(force_bytes(user.chercheur.utilisateur.pk)),
+        'token': tokenGenerator.make_token(user.chercheur.thesard.directeurThese.chercheur.utilisateur),
+    })
+    email = EmailMessage(
+                mail_subject, message, to=[toEmail]
+    )
+    email.content_subtype = "html"
+    email.send()
+
 
 def registerThesard(request:HttpRequest):
     if request.method == "POST":
@@ -73,14 +88,56 @@ def confirmEmail(request, uidb64, token):
     except(TypeError, ValueError, OverflowError, Chercheur.DoesNotExist):
         chercheur = None
         uid = None
-    if chercheur is not None and emailActivationToken.check_token(chercheur.utilisateur, token=token):
+    if chercheur is not None and tokenGenerator.check_token(chercheur.utilisateur, token=token):
         chercheur.emailValide = True
         chercheur.save()
         if chercheur.type == Chercheur.ChercheurType.THESARD:
-            # send notification to directeur de thése            
-            return HttpResponse("you are a thesard")
+            # send notification to directeur de thése
+            utilisateur =  chercheur.utilisateur
+            toEmail = chercheur.thesard.directeurThese.chercheur.utilisateur.email
+            sendThesardAccountConfirmationEmail(utilisateur, toEmail)
+            return render(request, 'utilisateurs/page-instruction-apres-validation-email-thesard.html')
         elif chercheur.type == Chercheur.ChercheurType.PROFESSEUR:
             # send notification to directeur de departement
             return HttpResponse("you are a professeur")
     else:
-        return HttpResponse(f"uid : {uid}, user uid: {chercheur.utilisateur.pk}, checktoken = {emailActivationToken.check_token(chercheur, token=token)}")
+        return HttpResponse(f"uid : {uid}, user uid: {chercheur.utilisateur.pk}, checktoken = {tokenGenerator.check_token(chercheur.utilisateur, token=token)}")
+
+def notifyAccountValidation(toEmail):
+    mail_subject = 'Valider Un compte thesard'
+    message = render_to_string('utilisateurs/message-validation-compte.html', {
+        'domain': '127.0.0.1:8000',
+    })
+    email = EmailMessage(
+                mail_subject, message, to=[toEmail]
+    )
+    email.content_subtype = "html"
+    email.send()
+
+
+def confirmThesardAccount(request, prof_uidb64, thesard_uidb64, token):
+    professeur = None
+    puid = None
+    thesard = None
+    tuid = None
+    try:
+        puid = uuid.UUID((force_str(urlsafe_base64_decode(prof_uidb64))))
+        professeur = Professeur.objects.get(chercheur__utilisateur__pk=puid)
+    except(TypeError, ValueError, OverflowError, Professeur.DoesNotExist, Thesard.DoesNotExist):
+        professeur = None
+        uid = None
+
+    try:
+        tuid = uuid.UUID((force_str(urlsafe_base64_decode(thesard_uidb64))))
+        thesard = Thesard.objects.get(chercheur__utilisateur__pk=tuid)
+    except(TypeError, ValueError, OverflowError, Professeur.DoesNotExist, Thesard.DoesNotExist):
+        professeur = None
+        uid = None
+
+    if professeur is not None and thesard is not None and tokenGenerator.check_token(professeur.chercheur.utilisateur, token=token):
+        thesard.chercheur.utilisateur.is_active = True
+        thesard.chercheur.utilisateur.save()
+        notifyAccountValidation(thesard.chercheur.utilisateur.email)
+        return render(request, 'utilisateurs/page-validation-compte.html')
+    else:
+        return HttpResponse(f"uid : {uid}, thesard: {thesard is not None}, professeur: {professeur is not None} checktoken = {tokenGenerator.check_token(professeur.chercheur.utilisateur, token=token)}")
