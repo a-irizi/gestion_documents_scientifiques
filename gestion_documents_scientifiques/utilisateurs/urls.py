8d83016cbd2bd15906d5f086ad8a91a54a88e4aa
a-irizi <irizi.anass.00@gmail.com>
2022-05-21 17:12:06 +0100

    Add url used in professeur account validation

--- gestion_documents_scientifiques/utilisateurs/urls.py
+++ gestion_documents_scientifiques/utilisateurs/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import confirmThesardAccount, register, registerProfesseur, registerThesard, confirmEmail
+from .views import confirmProfesseurAccount, confirmThesardAccount, register, registerProfesseur, registerThesard, confirmEmail
 
 urlpatterns = [
     path('register/', register, name="register"),
@@ -8,4 +8,6 @@ urlpatterns = [
     path('register/professeur/', registerProfesseur, name="register-professeur"),
     path('confirm-email/<uidb64>/<token>/', confirmEmail, name="confirm-email"),
     path('confirm-thesard-account/<prof_uidb64>/<thesard_uidb64>/<token>/', confirmThesardAccount, name="confirm-thesard-account"),
+    path('confirm-professeur-account/<directeurLabo_uidb64>/<prof_uidb64>/<token>/', confirmProfesseurAccount,name="confirm-professeur-account"),
 ]
+    
