cbd62faf8d73a3a75c2ab52c91e401d9f912c072
a-irizi <irizi.anass.00@gmail.com>
2022-05-21 15:02:49 +0100

    Add url for validating thesard account

--- gestion_documents_scientifiques/utilisateurs/urls.py
+++ gestion_documents_scientifiques/utilisateurs/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import register, registerProfesseur, registerThesard, confirmEmail
+from .views import confirmThesardAccount, register, registerProfesseur, registerThesard, confirmEmail
 
 urlpatterns = [
     path('register/', register, name="register"),
     path('register/thesard/', registerThesard, name="register-thesard"),
     path('register/professeur/', registerProfesseur, name="register-professeur"),
     path('confirm-email/<uidb64>/<token>/', confirmEmail, name="confirm-email"),
+    path('confirm-thesard-account/<prof_uidb64>/<thesard_uidb64>/<token>/', confirmThesardAccount, name="confirm-thesard-account"),
 ]
