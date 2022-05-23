d8b9218d852190527ebe17caea12ab594b04a994
a-irizi <irizi.anass.00@gmail.com>
2022-05-23 16:41:54 +0100

    Add form for PublicationRevueInternational

--- gestion_documents_scientifiques/papiers/forms.py
+++ gestion_documents_scientifiques/papiers/forms.py
@@ -1,2 +1,16 @@
 from django import forms
 from . import models
+
+
+class PublicationRevueInternationalForm(forms.ModelForm):
+    class Meta:
+        model = models.PublicationRevueInternational
+        fields = ['papier', 'titre', 'indexation', 'anneePublication',
+                  'nomJournal', 'numeroJournal', 'volumeJournal', 'pageDébut', 'pageFin', ]
+
+    def save(self, commit=True):
+        pub = super().save(commit = False)
+        pub.papierType = models.Papier.PapierType.PUBLICATION_REVUE_INTERNATIONAL
+        if commit:
+            pub.save()
+        return pub
