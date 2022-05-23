59a57f14c73d9817c8727dcf217623289f360ca7
a-irizi <irizi.anass.00@gmail.com>
2022-05-23 17:02:33 +0100

    Add form for ChapitreOuvrage

--- gestion_documents_scientifiques/papiers/forms.py
+++ gestion_documents_scientifiques/papiers/forms.py
@@ -9,8 +9,25 @@ class PublicationRevueInternationalForm(forms.ModelForm):
                   'nomJournal', 'numeroJournal', 'volumeJournal', 'pageDébut', 'pageFin', ]
 
     def save(self, commit=True):
-        pub = super().save(commit = False)
+        pub = super().save(commit=False)
         pub.papierType = models.Papier.PapierType.PUBLICATION_REVUE_INTERNATIONAL
         if commit:
             pub.save()
         return pub
+
+
+class ChapitreOuvrageForm(forms.ModelForm):
+    class Meta:
+        model = models.ChapitreOuvrage
+        fields = ['papier', 'titre', 'indexation', 'datePublication', 'nomOuvrage', 'EditionOuvrage', 'nomChapitre', 'pageDébut', 'pageFin',
+                  ]
+        widgets = {
+            'datePublication': forms.SelectDateWidget(),
+        }
+
+    def save(self, commit=True):
+        chap = super().save(commit=False)
+        chap.papierType = models.Papier.PapierType.CHAPITRE_OUVRAGE
+        if commit:
+            chap.save()
+        return chap
